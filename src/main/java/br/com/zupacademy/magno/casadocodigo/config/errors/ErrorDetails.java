12d1a3ef563406876e39aa0734065d07a02c1c5d
Magno Azevedo <magnoazneto@gmail.com>
2021-04-19 10:02:32 -0300

    adiciona exception handler

---
+++ src/main/java/br/com/zupacademy/magno/casadocodigo/config/errors/ErrorDetails.java
@@ -0,0 +1,20 @@
+package br.com.zupacademy.magno.casadocodigo.config.errors;
+
+public class ErrorDetails {
+
+    private String reason;
+    private String error;
+
+    public ErrorDetails(String reason, String error) {
+        this.reason = reason;
+        this.error = error;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public String getError() {
+        return error;
+    }
+}
