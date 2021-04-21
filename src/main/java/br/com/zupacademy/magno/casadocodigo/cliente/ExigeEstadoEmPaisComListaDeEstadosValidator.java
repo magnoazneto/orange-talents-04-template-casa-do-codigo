85330921869c91add60b63f9f50a79d221164f7b
magnoazneto <magnoazneto@gmail.com>
2021-04-21 16:13:57 -0300

    melhora validador para fazer apenas 1 busca no banco e corrige segundo passo da validacao

--- src/main/java/br/com/zupacademy/magno/casadocodigo/cliente/ExigeEstadoEmPaisComListaDeEstadosValidator.java
+++ src/main/java/br/com/zupacademy/magno/casadocodigo/cliente/ExigeEstadoEmPaisComListaDeEstadosValidator.java
@@ -1,7 +1,6 @@
 package br.com.zupacademy.magno.casadocodigo.cliente;
 
-import br.com.zupacademy.magno.casadocodigo.estado.EstadoRequest;
-import br.com.zupacademy.magno.casadocodigo.pais.Pais;
+import br.com.zupacademy.magno.casadocodigo.estado.Estado;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 import org.springframework.validation.Errors;
@@ -11,6 +10,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 // CI total: 7 pts
@@ -30,24 +30,24 @@ public class ExigeEstadoEmPaisComListaDeEstadosValidator implements Validator {
             return;
         }
 
-        // recuperando pais informado (que já passou por validacao @ExistsValue)
-        // mas mesmo assim existem Asserts para programacao defensiva
         ClienteRequest request = (ClienteRequest) target; // 1 pela classe ClienteRequest
-        Query paisInformadoQuery = manager.createQuery("select p from Pais p where p.id = :pPaisId");
-        Assert.notNull(request.getPaisId(), "Id do país não deveria ser nulo"); // 1 ponto pela condicao do assert?
-        paisInformadoQuery.setParameter("pPaisId", request.getPaisId());
-        List<?> paisInformadoResult = paisInformadoQuery.getResultList();
-        Assert.state(paisInformadoResult.size() == 1, "Deveria existir 1, e apenas 1, país para o Id informado"); // 1
-        Pais paisInformado = (Pais) paisInformadoResult.get(0); // 1 pela classe Pais
 
-        // verificando se o pais tem cadastro de estados
-        Query query = manager.createQuery("select e from Estado e where e.pais = :pPais");
-        // select e from Estado e join e.pais p where p.id =:pPaisId
-        query.setParameter("pPais", paisInformado);
+        // verificando se o pais tem cadastro de estados, e se tiver, se o estado existe dentro daquele pais
+        Query query = manager.createQuery("select e from Estado e join e.pais p where p.id =:pPaisId");
+        Assert.notNull(request.getPaisId(), "O Id do País não deveria ser nulo"); // 1 ponto pela condicional, embora seja simples (teria que ver com o time)
+        query.setParameter("pPaisId", request.getPaisId());
         List<?> estadosEncontrados = query.getResultList();
+
         if(!estadosEncontrados.isEmpty() && request.getEstadoId() == null){ // 2 - um para cada condicao
             errors.rejectValue("estadoId", null, "O Estado é obrigatório quando o País informado tem Estados existentes");
+            return;
         }
 
+        // +1 pela classe de Estado
+        List<Long> estadosIds = estadosEncontrados.stream().map(estado -> ((Estado) estado).getId()).collect(Collectors.toList());
+
+        if(!estadosIds.contains(request.getEstadoId())){ // 1 pela condicao
+            errors.rejectValue("estadoId", null, "O Estado informado não existe no País informado");
+        }
     }
 }
